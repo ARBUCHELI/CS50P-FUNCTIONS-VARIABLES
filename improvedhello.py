c5b06216b553bee85429c42e6f0abb0c996f5d0b
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-05 10:54:40 +0300

    refactoring improvedhello.py

--- improvedhello.py
+++ improvedhello.py
@@ -2,6 +2,7 @@
 name = input("What's your name? ")
 
 # Print hello and the inputted name
-#print("hello, " + name)
-#Just take into consideration that some functions, for example print, can take multiple arguments.
+# print("hello, " + name)
+# Just take into consideration that some functions, for example print, can take multiple arguments. In this case PYthon, add the extra
+# space.
 print("hello,", name)
