dee863f475caa58e0d4a5eb3f68a43725c646adc
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-16 15:12:48 +0300

    floats

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#x = 1
+#y = 2
+
+#z = x + y
+
+#print(z)
+
+# More interactive version
+#x = input("What's x? ")
+#y = input("What's y? ")
+
+#z = int(x) + int(y)
+
+#print(z)
+
+# Even more interactive version
+#x = int(input("What's x? "))
+#y = int(input("What's y? "))
+
+#print(x + y)
+
+# Making a calculator that works with floats
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+print(x + y)
